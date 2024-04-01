6629e25e69a0fe7d5c62295615b50100b31716e8
Madlena Blajin <68249413+MadlenaB@users.noreply.github.com>
2024-04-01 16:42:48 +0300

    Update functii_tren.c

--- functii_tren.c
+++ functii_tren.c
@@ -1,15 +1,25 @@
 /*--- Functiile implementate pentru UPDATE, SEARCH, QUERY AND EXECUTE----*/
 #include "tren.h"
 
+TLista AlocaCelula(char x){
+    TLista aux = (TLista)malloc(sizeof(TCelula));
+    if(!aux) = return NULL;
+    aux->info = x;
+    aux->urma = NULL;
+    return aux;
+}
 TTren* InitT(){
-    TTren *t; /*t - va fi trenuletul nostru - o lista circulara dubla inlantuita*/
+    TTren *t; /* t - va fi trenuletul nostru - o lista circulara dubla inlantuita */
     t = (TTren*)malloc(sizeof(TTren));
     if( !t ) return NULL;  
     TLista *s = (TLista)malloc(sizeof(TLista));
     if( !s ) return NULL;
+    TLista aux = AlocaCelula(#);
     t->s->info = 0;
-    t->s->urm = s;
-    t->s->pre = s;
+    t->s->urm = aux;
+    t->s->pre = aux;
+    aux->urm = t->s;
+    aux->pre = t->s;
     t->mecanic = t->s;
     return t;
 }
@@ -23,5 +33,4 @@ TCoada* InitQ(){
 }
 
 //int InsertOperationQue(Tcoada *c, )
-
 //void MOVE_LEFT(TTrain t, TCoada c )
