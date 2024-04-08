ccf2a41ca284a7f464953f1279e63be47a577792
Madlena Blajin <68249413+MadlenaB@users.noreply.github.com>
2024-04-08 16:09:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include "tren.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
+    FILE *in = fopen("tema1.in","r");
+    FILE *out = fopen("tema1.out","w");
+    if(in == NULL){
+        printf("File inexistent\n");
+        return 1;
+    }
+    int numarOperatii = 0;
+    fscanf(in, "%d\n", &numarOperatii);
+    printf("%d\n", numarOperatii);
+    TTren *tren = InitT();
+    TCoadaOp *coadaOp = InitQ();
 
+    char op[200];
+    char val[10];
+    for (int i = 0; i < numarOperatii; i++) {
+        fscanf(in, "%s", op);
+        printf("%s\n", op);
+        if(strcmp(op, "SHOW_CURRENT") == 0){
+             SHOW_CURRENT(tren, out);
+        }else if(strcmp(op, "SHOW") == 0){
+             SHOW(tren, out);
+        }else if(strcmp(op, "SWITCH") == 0){
+             SWITCH(coadaOp);
+        }
+        else if(strcmp(op, "EXECUTE") != 0){
+            strcpy(val, "");
+            fscanf(in, " %s\n", val);
+            printf("Valoarea este:%s\n", val);
+            IntrQ(coadaOp, op, val); /*Introducem in coada operatiile din fisier*/
+        }
+        if (strcmp(op, "EXECUTE") == 0) {
+            EXECUTE(tren, coadaOp, out);
+        }
+    }
+    //AfisareQ(coadaOp);
+    //DistrQ(&coadaOp);
+    fclose(in);
+    fclose(out);
 return 0;
 }
